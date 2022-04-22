c7f3f2b97538e33eed0d9fe1e8bd14172ef56199
linm1 <linm1@bxscience.edu>
2022-04-22 02:48:55 -0400

    Basic UI Implementation

---
+++ lib/const.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color drawer = Color(0xFFebded2);
+const Color frontPage = Color(0xFFe7dece);
+const Color background = Color(0xFFada595);
+const Color frontPageShade = Color(0xFF2e2c29);
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,142 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'package:flutter_cube/flutter_cube.dart';
+import 'const.dart' as Constants;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+    title: "BMI Calculator",
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  final double maxSlide = 300.0;
+  late Object person;
+  late Scene _scene;
+  late Object title;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 250),
+    )..addListener(() {
+        person.rotation.y = (180 * _animationController.value / 2);
+        person.position.x = 4.5 * _animationController.value;
+        person.position.z = 0 * (_animationController.value);
+        person.scale.y = 10 + (1.5 * _animationController.value);
+        //person.position.y = 0.3 * (_animationController.value);
+
+        title.rotation.y = (180 * _animationController.value / 2);
+        title.position.x = (3.5 * _animationController.value) - 0.5;
+        title.position.z = 0 * (_animationController.value) - 1;
+        title.position.y = 0.9 * (_animationController.value) + 4.5;
+        person.updateTransform();
+        title.updateTransform();
+
+        _scene.update();
+      });
+    person = (Object(
+        fileName: 'assets/Chimpanzee/chimp.obj',
+        scale: Vector3(10.0, 10.0, 10.0)));
+    title = (Object(
+      fileName: 'assets/title/bmitext1.obj',
+      scale: Vector3(5, 5, 5),
+    ));
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void toggle() {
+    _animationController.isDismissed
+        ? _animationController.forward()
+        : _animationController.reverse();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    var myDrawer = Container(
+      height: MediaQuery.of(context).size.height,
+      //1.26
+
+      color: Constants.drawer,
     );
+    var myChild = Cube(
+      onSceneCreated: (Scene scene) {
+        _scene = scene;
+        _scene.camera.position.z = 25;
+        _scene.camera.target.y = 1.0;
+        _scene.camera.zoom = 0.96;
+        _scene.camera.fov = 30;
+        _scene.world.add(person);
+        _scene.world.add(title);
+        person.rotation.x = -1;
+        person.updateTransform();
+        title.position.setValues(-0.5, 4.5, -1);
+        title.updateTransform();
+        _scene.update();
+        _scene.camera.target;
+      },
+    );
+//shading as the front page turns
+    Animatable<Color?> background = TweenSequence<Color?>([
+      TweenSequenceItem(
+          tween: ColorTween(
+              begin: Constants.frontPage, end: Constants.frontPageShade),
+          weight: 1)
+    ]);
+
+    return GestureDetector(
+        onTap: toggle,
+        child: AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, _) {
+            return Container(
+                //clipBehavior: Clip.antiAliasWithSaveLayer,
+                color: Constants.background,
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  children: <Widget>[
+                    Transform.translate(
+                      //1.82
+                      offset: Offset(
+                          maxSlide * (_animationController.value - 1.22), 0),
+                      child: Transform(
+                          child: myDrawer,
+                          alignment: Alignment.centerRight,
+                          transform: Matrix4.identity()
+                            ..setEntry(3, 2, 0.001)
+                            ..rotateY((math.pi / 2 * 0.1) *
+                                (_animationController.value))
+                            ..setEntry(1, 1, 1.1)),
+                    ),
+                    Transform.translate(
+                      offset: Offset(300 * (_animationController.value), 0),
+                      child: Transform(
+                          child: Container(
+                            color: background.evaluate(AlwaysStoppedAnimation(
+                                _animationController.value)),
+                            height: MediaQuery.of(context).size.height * 1.2,
+                          ),
+                          alignment: Alignment.centerLeft,
+                          transform: Matrix4.identity()
+                            ..scaled(1.5)
+                            ..setEntry(3, 2, 0.001)
+                            ..rotateY(-(math.pi / 2 + 0.1) *
+                                _animationController.value)
+                            ..setEntry(1, 1, 1.5)),
+                    ),
+                    myChild,
+                  ],
+                ));
+          },
+        ));
   }
 }
